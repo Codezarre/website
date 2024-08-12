e473d8728d2172ca3ff13815272e7633894f9b49
Codezarre <codezarre@gmail.com>
2024-08-12 03:20:30 +0200

    Rotate social icons on hover

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,11 +8,11 @@
 			class="
 			brightness-150 text-transparent text-6xl
 			bg-clip-text bg-gradient bg-gradient-to-r
-			from-[#50A3F2] to-[#2A489D] drop-shadow-xl"
+			from-[#50A3F2] to-[#2A489D]"
 		>
 			Codezarre.
 		</h1>
-		<span class="flex gap-5 justify-center drop-shadow-xl">
+		<span class="flex gap-5 justify-center">
 			<a href="/youtube"><Icon.YoutubeBrand size="30" /></a>
 			<a href="/github"><Icon.GithubBrand size="30" /></a>
 			<a href="/x"><Icon.XTwitterBrand size="30" /></a>
@@ -21,3 +21,14 @@
 		</span>
 	</article>
 </main>
+
+<style>
+	a {
+		transition: transform 2.5s;
+	}
+
+	a:hover {
+		transition: transform 1s;
+		transform: rotate(360deg);
+	}
+</style>
