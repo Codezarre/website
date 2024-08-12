68c6e5d7a6599cc786dd353434794411693e2bf6
Codezarre <codezarre@gmail.com>
2024-08-12 03:19:08 +0200

    Redirect on error

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	onMount(() => goto('/'));
+</script>
+
+<main>
+	<article class="prose prose-invert text-center">
+		<h1 class="text-6xl text-red-700 animate-pulse">Codezerror?</h1>
+	</article>
+</main>
